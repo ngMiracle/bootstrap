7dbb2020897aa39e9317b0b19ce7d100ff0a7838
Trotyl <trotyl@qq.com>
2017-03-19 13:10:56 +0800

    feat: add test for container directive

---
+++ src/core/directives/container.directive.spec.ts
@@ -0,0 +1,48 @@
+import { Component, DebugElement } from '@angular/core'
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser'
+
+import { ContainerDirective } from './container.directive';
+
+@Component({
+  template: `
+    <div [bsContainer]="containerType">Content</div>
+  `
+})
+class TestComponent {
+  containerType: string | null
+}
+
+describe('ContainerDirective', () => {
+  let fixture: ComponentFixture<TestComponent>
+  let debugElement: DebugElement
+  let component: TestComponent
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        TestComponent,
+        ContainerDirective
+      ],
+    }).compileComponents()
+  }))
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent)
+    debugElement = fixture.debugElement.query(By.directive(ContainerDirective))
+    component = fixture.componentInstance
+
+    fixture.detectChanges()
+  })
+
+  it(`should have 'container' class by default`, async(() => {
+    expect(debugElement.classes['container']).toBeTruthy()
+  }))
+
+  it(`should have 'container-fluid' class when set to fluid`, async(() => {
+    component.containerType = 'fluid'
+    fixture.detectChanges()
+
+    expect(debugElement.classes['container-fluid']).toBeTruthy()
+  }))
+})
