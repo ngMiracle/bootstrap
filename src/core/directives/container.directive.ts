46d3ae033e10a2080272500ba2c6e270c72dd89c
Trotyl <trotyl@qq.com>
2017-03-19 12:24:37 +0800

    feat: add fluid container support for container directive

--- src/core/directives/container.directive.ts
+++ src/core/directives/container.directive.ts
@@ -8,7 +8,16 @@ export class ContainerDirective {
   type: string | null
 
   @HostBinding('class.container')
-  get hasContainerClass() {
-    return this.type !== 'fluid'
+  get hasContainerClass(): boolean {
+    return !this.isFluid
+  }
+
+  @HostBinding('class.container-fluid')
+  get hasContainerFluidClass(): boolean {
+    return this.isFluid
+  }
+
+  private get isFluid(): boolean {
+    return this.type === 'fluid'
   }
 }
